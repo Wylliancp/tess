cae109e4f13672cb8ef5c65fa98150453a57730f
Wyllian Costa Pereira <wyllianpereira@funcef.com.br>
2018-08-17 18:30:06 -0300

    Tratamento das páginas de Erro

---
+++ tess/Erro/ErrorsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tess.Erro
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult General(Exception exception)
+        {
+            return View();
+        }
+
+        public ActionResult Http400()
+        {
+            return View();
+        }
+
+        public ActionResult Http404()
+        {
+            return View();
+        }
+
+    }
+}
